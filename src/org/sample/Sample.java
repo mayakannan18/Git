59a9708c0e91cdcf9031617948fc834a6002aa77
mayakannan18 <mayakannanmk18@gmail.com>
2022-08-29 08:04:13 -0700

    PROJECT BEGINS

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,5 +1,8 @@
 package org.sample;
 
 public class Sample {
-
+	public static void main(String[] args) {
+		System.out.println("SUCCESFULLY CREATED ");
+		System.out.println("GIT CREATED ");
+	}
 }
