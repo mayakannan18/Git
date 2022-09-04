62594ab81ad7ce213f9d2e2e50b4b360aa3165b4
mayakannan18 <mayakannanmk18@gmail.com>
2022-09-04 02:32:53 -0700

    Added By Vembu

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -4,5 +4,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("SUCCESFULLY CREATED ");
 		System.out.println("GIT CREATED ");
+		System.out.println("Created to Mk");
+		System.out.println("Created to SK");
+		System.out.println("Created to Vembu");
 	}
 }
