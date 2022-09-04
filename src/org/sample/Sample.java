e9d17e97d724d98f0653be7d712a79feaf670d0c
mayakannan18 <mayakannanmk18@gmail.com>
2022-09-04 02:17:10 -0700

    Added by SK

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("SUCCESFULLY CREATED ");
 		System.out.println("GIT CREATED ");
+		System.out.println("Master to SK");
 	}
 }
